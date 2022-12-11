aa632b7681de585d36a7a1f5e57f2fef034a887f
heliababz <44245561+heliababz@users.noreply.github.com>
2022-12-11 13:45:35 -0600

    Update eratosthenes.py

--- Erathosthenes/eratosthenes.py
+++ Erathosthenes/eratosthenes.py
@@ -9,21 +9,19 @@ def eratosthenes(n):
   could_be_prime[0] = False
   could_be_prime[1] = False
 
-  # Biggest f
-actor we need to cross out
+  # Lagest factor that needs to be crossed out
   max_factor = int(math.sqrt(n))
 
   # For all j up to max_factor, cross out
   for j in range(max_factor+1):
     # Only cross out multiples of j if j is prime
     if could_be_prime[j]: 
-      crossout = j+j # The first thing we need to cross out is 2j.
+      crossout = j+j # The first thing we need to cross out is 2j
       while(crossout <= n):
         could_be_prime[crossout] = False
-        crossout += j # Note the speed-up: we find the multiples of j by repeatedly adding j,
-                      # not explicitly multiplying out 2*j, 3*j, 4*j, 5*j,...
+        crossout += j # Note: addition is much faster than multiplication
 
-  # Now turn into a list of primes.
+  # Clean up: put the remaining numbers in could_be_prime into a list of primes
   prime_list = [j for j in range(n+1) if could_be_prime[j]]
 
   return prime_list
